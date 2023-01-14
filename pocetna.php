f7f87b53cc94a9bf48f232feff61f25134090dc9
Sofija Djukic <sofijadjukic@Sofijas-MacBook-Air.local>
2023-01-14 17:28:25 +0100

    main page

---
+++ pocetna.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="stillogreg.css">
+</head>
+<body>
+<div class="header" id="h2">
+    <h2> Cooking for dummies</h2>
+</div>
+<div class="sredina">
+
+
+            <div class="red">
+                <img src="cooking.jpeg" alt="rec"  style="width:100%" class="image"> 
+            <div class="textover"> <div class="Text"><a href="novipost.php">Add a recipe </a></div> </div>
+            </div>
+
+
+            <div  class="red" >
+                <img src="restoran.jpeg" style="width:100%" class="image">
+                <div class="textover"> <div class="Text"><a href="https://www.canva.com/menus/templates/meal-planner/">Find a deal </a></div> </div>
+            </div>
+
+            </div>
+
+</body>
+</html>
