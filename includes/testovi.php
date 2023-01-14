88626fecbad530553cbd6fcebbb86a01db7fba12
Sofija Djukic <sofijadjukic@Sofijas-MacBook-Air.local>
2023-01-14 12:44:34 +0100

    testovi part2

--- includes/testovi.php
+++ includes/testovi.php
@@ -62,3 +62,62 @@ function AddNewUser($conn,$ime,$mejl,$lozinka) {
 
 //////////////////////////////////////////////
 
+function missingfieldlog($ime,$lozinka) {
+    $fin;
+    if (empty($ime)||empty($lozinka)) {$fin=true;}
+ else {$fin=false;}
+ return $fin;
+}
+
+function  ExistingUser($conn, $ime, $lozinka){
+    $validusername= sameun($conn, $ime); 
+
+    if ($validusername === false) { header("location: ../login.php?error=UsernameNotFound");
+    exit();
+}
+
+
+$skrivenalozinka= $validusername["upassword"];
+$tacnalozinka= password_verify($lozinka,$skrivenalozinka);
+if ($tacnalozinka === false) {
+    header("location: ../login.php?error=wrongpassword");
+    exit();
+}
+else if ($tacnalozinka === true) {
+    session_start();
+    // session variables - super variable
+    $_SESSION["sessid"] = $validusername["id"];
+    $_SESSION["sessusername"] = $validusername["uusername"]; //username
+    header("location: ../pocetna.php");
+    exit();
+}
+}
+
+
+///////////////////////
+
+function missingfieldrec($naslov,$tekst) {
+    $fin;
+    if (empty($naslov)||empty($tekst)) {$fin=true;}
+ else {$fin=false;}
+ return $fin;
+}
+
+function AddNewPost($conn, $naslov, $tekst, $kategorija) {
+    $tabela = "INSERT INTO noviartikal (title,tekst,kategorija) VALUES (?, ?, ?);" ;
+    $stmt= mysqli_stmt_init($conn); //using prepared stmt to make it more secure- user can write code in input and change our db
+    if (!mysqli_stmt_prepare($stmt, $tabela)){
+        header("location: ../signup.php?error=fejlovde3");
+    exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "sss", $naslov, $tekst, $kategorija); // data bound to the stms
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    header("location: ../login.php");
+    exit();
+}
+
+
+
+?>
