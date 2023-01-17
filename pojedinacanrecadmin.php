97c455b5d80d83085ab3775cc03c80e73179e18c
Sofija Djukic <sofijadjukic@Sofijas-MacBook-Air.local>
2023-01-17 18:25:21 +0100

    admin individual rec pg

---
+++ pojedinacanrecadmin.php
@@ -0,0 +1,32 @@
+<?php
+include "includes/pojedinacanrecadminDODATNO.php"
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="stillogreg.css">
+<link rel="stylesheet" href="pojedinacan.css">
+</head>
+<body>
+<div class="header">
+            <ul>
+                <li> <a href="recepti.php">Our recipes :)</a></li>
+                <li> <a href="pocetna.php">Main Page</a></li>
+                <li> <a id="aboutus.php" href="login.html">AboutUs</a></li>
+                            </ul>          
+</div>
+
+<?php foreach($query as $q) {?>
+<div class="tekstito">
+    <div class="nasl">
+<h5> <?php  echo $q['title'] ?> </h5> </div>
+<p> <?php  echo $q['tekst'] ?> </p> </div>
+<form method="POST">
+    <input type="text" hidden name="id" value="<?php echo $q["rid"];?>">
+    <button name="delete"> Delete </button>
+</form> 
+<?php } ?>
+</body>
+</html>
