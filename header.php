50c14660d07ce306efa8248d3eacb828b2e905ab
Sofija Djukic <sofijadjukic@Sofijas-MacBook-Air.local>
2023-01-18 23:10:15 +0100

    header

---
+++ header.php
@@ -0,0 +1,11 @@
+<div class="header">
+            <ul>
+                
+                <li> <a href="pocetna.php">Main Page</a></li>
+                <li> <a id="aboutus.php" href="login.html">AboutUs</a></li>
+                <li> <a  href="includes/logout.php">Log out</a></li>
+                <li> <a  href="deals.php"> Our deals</a> </li>
+                
+                          
+            
+</div>
