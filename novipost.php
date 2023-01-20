306d1c6c4805d04f8c9b4aa38fb350644252994d
Sofija Djukic <sofijadjukic@Sofijas-MacBook-Air.local>
2023-01-20 09:34:51 +0100

    linked to the shared header and removed the individual one

--- novipost.php
+++ novipost.php
@@ -1,4 +1,5 @@
 <?php
+include "hdr.php"
 ?>
 
 <!DOCTYPE html>
@@ -14,22 +15,7 @@
 
 <body>
 
-<div class="header">
-            <ul>
-                <li> <a href="recepti.php">Our recipes :)</a></li>
-                <li> <a href="pocetna.php">Main Page</a></li>
-                <li> <a id="aboutus.php" href="login.html">AboutUs</a></li>
-                <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Categories</a>
-    <div class="dropdown-content">
-    <a href="kategorije.php?kat=vegan">Vegan</a>
-    <a href="kategorije.php?kat=glutenfree">Gluten free 2</a>
-    <a href="kategorije.php?kat=underten"> Under 10$</a>
-    <a href="kategorije.php?kat=underfiveminutes"> Under 5 minutes</a>
-  </div> 
-  </li>
-                            </ul>          
-</div>
+
 
 
     <div class=centar>
