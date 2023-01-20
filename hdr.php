bc6f34d798989984a2529849a047910d74647bcd
Sofija Djukic <sofijadjukic@Sofijas-MacBook-Air.local>
2023-01-20 08:43:16 +0100

    header for recipe side of student site

---
+++ hdr.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<div class="header">
+            <ul>
+                <li> <a href="pocetna.php">Main Page</a></li>
+                <li> <a href="recepti.php">Our recipes :)</a></li>
+                <li> <a id="aboutus.php" href="login.html">AboutUs</a></li>
+                <li> <a  href="includes/logout.php">Log out</a></li>
+                <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Categories</a>
+    <div class="dropdown-content">
+    <a href="kategorije.php?kat=vegan">Vegan</a>
+    <a href="kategorije.php?kat=glutenfree">Gluten free 2</a>
+    <a href="kategorije.php?kat=underten"> Under 10$</a>
+    <a href="kategorije.php?kat=underfiveminutes"> Under 5 minutes</a>
+  </div> 
+  </li>
+                            </ul>
+                          
+            
+</div>
